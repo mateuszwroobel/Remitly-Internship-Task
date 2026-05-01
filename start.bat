a6dcdc2e55d1d4b4442702ecb4bbf9df3d9c8e23
mateusz <mateusz.wrobel005@gmail.com>
2026-05-01 13:28:58 +0200

    clean up

--- start.bat
+++ start.bat
@@ -8,9 +8,9 @@ docker compose up --build -d
 
 echo.
 echo ==========================================================
-echo [ROCKET] Stock Exchange Cluster is running inside Docker!
-echo [ANTENNA] Load Balancer is available at: http://localhost:%PORT%
-echo [NETWORK] 3 Backend Nodes are peering internally.
-echo [STOP] To stop the cluster: docker compose down
-echo [LOGS] To view live logs: docker compose logs -f
+echo Stock Exchange Cluster is running inside Docker!
+echo Load Balancer is available at: http://localhost:%PORT%
+echo 3 Backend Nodes are peering internally.
+echo To stop the cluster: docker compose down
+echo To view live logs: docker compose logs -f
 echo ==========================================================
